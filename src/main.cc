981d5b4404e22efb310fe0419f3f285003baafdd
darkyboys <darkyboys726@gmail.com>
2025-03-27 23:48:06 +0530

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -10,7 +10,7 @@ int main (int argc , char * argv[]){
     if (argc == 1) std::cout <<"CASCI 2.0\n", std::exit ( 3 );
     if (argc == 3) std::cout <<"Error -> CASCI Need a pattern tooo!!!!\n", std::exit ( 3 );
     if (argc == 2) std::cout <<"Error -> CASCI Need a file name and a pattern tooo!!!!\n", std::exit ( 3 );
-    if (argc == 5) out_path = argv[4], std::cout <<"'"<<out_path<<"'\n";
+    if (argc == 4) out_path = argv[3], std::cout <<"'"<<out_path<<"'\n";
     
     else {
         if (std::string(argv[1]) == "encrypt" or std::string(argv[1]) == "e"){
